3b6d790e656600a50a5a52067998784027fe22a2
Greeg Titan <mistzero0@gmail.com>
2025-02-05 14:14:55 +0700

    modify type used and sizing

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-import yargs from "yargs/yargs";
-import { hideBin } from "yargs/helpers";
-import path from "path";
-import fs from "fs";
-import { turnToWebp } from "./lib.js";
+#!/usr/bin/env node
+
+import yargs from 'yargs/yargs';
+import { hideBin } from 'yargs/helpers';
+import path from 'path';
+import fs from 'fs';
+import { turnToWebp } from './lib.js';
 
 const argv = yargs(hideBin(process.argv)).argv;
 const cwd = process.cwd();
@@ -10,57 +12,58 @@ const fullInputPath = process.argv[2] || argv.f || argv.file;
 
 const full = argv.full;
 const jpg = argv.jpg || argv.jpeg;
+const type = argv.type ?? 'avif';
 const name = argv.name;
-const aspect = argv.aspect || "portrait";
+const aspect = argv.aspect || 'portrait';
 
 if (!fullInputPath) {
-  console.log("Specify File Path!");
-  process.exit(1);
+	console.log('Specify File Path!');
+	process.exit(1);
 }
 
 if (fs.existsSync(fullInputPath)) {
-  if (fs.statSync(fullInputPath).isFile()) {
-    let outputName = String(argv.output || fullInputPath).split(".");
-    if (outputName.length > 1) outputName = outputName.slice(0, -1);
-    outputName = outputName.join(".");
-    const fullPath = path.join(cwd, fullInputPath);
+	if (fs.statSync(fullInputPath).isFile()) {
+		let outputName = String(argv.output || fullInputPath).split('.');
+		if (outputName.length > 1) outputName = outputName.slice(0, -1);
+		outputName = outputName.join('.');
+		const fullPath = path.join(cwd, fullInputPath);
 
-    try {
-      turnToWebp({ filePath: fullPath, full, aspect });
-    } catch (err) {
-      console.log(err);
-    }
-  } else if (fs.stat(fullInputPath).isDirectory()) {
-    const files = fs.readdir(fullInputPath, { encoding: "utf8" });
-    const destDir = path.join(fullInputPath, "COMPRESSED");
-    if (!fs.existsSync(destDir)) fs.mkdirSync(destDir);
+		try {
+			turnToWebp({ filePath: fullPath, full, aspect });
+		} catch (err) {
+			console.log(err);
+		}
+	} else if (fs.statSync(fullInputPath).isDirectory()) {
+		const files = fs.readdirSync(fullInputPath, { encoding: 'utf8' });
+		const destDir = path.join(fullInputPath, 'COMPRESSED');
+		if (!fs.existsSync(destDir)) fs.mkdirSync(destDir);
 
-    files.forEach((fileName, i) => {
-      const filePath = path.join(fullInputPath, fileName);
+		files.forEach((fileName, i) => {
+			const filePath = path.join(fullInputPath, fileName);
 
-      if (
-        fs.statSync(filePath).isFile() &&
-        /\.(jpg|jpeg|png)$/i.test(fileName)
-      ) {
-        try {
-          turnToWebp({
-            filePath,
-            dest: destDir,
-            full,
-            jpg,
-            // Name based on desired name and index
-            // name: name + " " + (i + 1),
-            name,
-            index: i + 1,
-          });
-        } catch (err) {
-          console.log(err);
-        }
-      }
-    });
-  } else {
-    console.log("Unsupported file type or folder structure.");
-  }
+			if (
+				fs.statSync(filePath).isFile() &&
+				/\.(jpg|jpeg|png)$/i.test(fileName)
+			) {
+				try {
+					turnToWebp({
+						filePath,
+						dest: destDir,
+						full,
+						type,
+						// Name based on desired name and index
+						// name: name + " " + (i + 1),
+						name,
+						index: i + 1,
+					});
+				} catch (err) {
+					console.log(err);
+				}
+			}
+		});
+	} else {
+		console.log('Unsupported file type or folder structure.');
+	}
 } else {
-  console.log("File or folder not found.");
+	console.log('File or folder not found.');
 }
--- lib.js
+++ lib.js
@@ -1,71 +1,66 @@
-import path from "path";
-import fs from "fs";
-import sharp from "sharp";
+import path from 'path';
+import fs from 'fs';
+import sharp from 'sharp';
 
-export async function turnToWebp({
-  filePath,
-  dest,
-  full,
-  jpg,
-  name,
-  index,
-}) {
-  const { fileName, file } = getOutputAndFile({
-    filePath,
-    dest,
-    jpg,
-    name,
-    index,
-  });
+export async function turnToWebp({ filePath, dest, full, type, name, index }) {
+	const { fileName, file } = getOutputAndFile({
+		filePath,
+		dest,
+		type,
+		name,
+		index,
+	});
 
-  const imageAspect = {
-    // portrait: { width: 1080 },
-    // landscape: { width: 1920 },
-    portrait: { height: 2000 },
-    landscape: { width: 2000 },
-  };
-  let item = sharp(file);
+	const imageAspect = {
+		// portrait: { width: 1080 },
+		// landscape: { width: 1920 },
+		portrait: { height: 2048 },
+		landscape: { width: 2048 },
+	};
+	let item = sharp(file);
 
-  const metadata = await item.metadata();
-  const aspect = metadata.width > metadata.height ? "landscape" : "portrait";
+	const metadata = await item.metadata();
+	const aspect = metadata.width > metadata.height ? 'landscape' : 'portrait';
 
-  if (full) {
-    // item = item.jpeg({ quality: 80 });
-    if (metadata.width >= 2000 || metadata.height >= 2000) {
-      item = item.resize({ ...imageAspect[aspect] });
-    }
-  }
+	if (full) {
+		// item = item.jpeg({ quality: 80 });
+		if (metadata.width >= 2000 || metadata.height >= 2000) {
+			item = item.resize({ ...imageAspect[aspect] });
+		}
+	}
 
-  item = item.withMetadata({
-    title: "Your Title",
-    description: "Your Description",
-    copyright: "Your Copyright",
-    comment: "Your Comment", // Some software may use 'comment' as a field
-  });
+	item = item.withMetadata({
+		title: 'Your Title',
+		description: 'Your Description',
+		copyright: 'Your Copyright',
+		comment: 'Your Comment', // Some software may use 'comment' as a field
+	});
 
-  if (jpg) {
-    item = item.jpeg();
-  } else {
-    item = item.avif();
-  }
+	if (type === 'jpg' || type === 'jpeg') {
+		item = item.jpeg();
+	} else if (type === 'webp') {
+		item = item.webp();
+	} else {
+		item = item.avif();
+	}
 
-  item.toFile(fileName, (err, info) => {
-    if (err) {
-      console.log("Error converting file");
-    }
-    console.log("Compressing file success!");
-  });
-};
+	item.toFile(fileName, (err, info) => {
+		if (err) {
+			console.log('Error converting file');
+		}
+		console.log('Compressing file success!');
+	});
+}
 
-export function getOutputAndFile({ filePath, dest, jpg, name, index }) {
-  const inputName = path.parse(filePath).name;
-  name = `${name ? name + index : inputName}.${jpg ? "jpeg" : "avif"}`;
+export function getOutputAndFile({ filePath, dest, type, name, index }) {
+	const inputName = path.parse(filePath).name;
+	name = `${name ? name + ' ' + index : inputName}.${type}`;
 
-  if (dest) {
-    name = path.join(dest, name);
-  }
+	if (dest) {
+		name = path.join(dest, name);
+	}
 
-  const file = fs.readFileSync(filePath);
+	const file = fs.readFileSync(filePath);
 
-  return { fileName: name, file };
+	return { fileName: name, file };
 }
